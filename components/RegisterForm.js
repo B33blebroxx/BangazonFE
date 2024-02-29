bdea3d134cf89238ce8df80b23c83b8a0916cb8c
B33blebroxx <b33blebroxx@gmail.com>
2024-02-28 20:04:30 -0600

    Setup and connection to backend complete

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -3,24 +3,59 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
+import { useAuth } from '../utils/context/authContext';
 
-function RegisterForm({ user, updateUser }) {
+function RegisterForm() {
+  const { user, updateUser } = useAuth();
   const [formData, setFormData] = useState({
-    bio: '',
-    uid: user.uid,
+    uId: user.fbUser.Uid,
+    firstName: '',
+    lastname: '',
+    email: '',
+    userName: '',
+    address: '',
+    isSeller: false,
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    registerUser(formData).then(() => updateUser(user.uid));
+
+    registerUser(formData).then(() => updateUser(user.fbUser.Uid));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Label>FirstName</Form.Label>
+        <Form.Control type="text" name="FirstName" required placeholder="First Name" onChange={handleChange} />
+        <Form.Text className="text-muted">Please enter your first name</Form.Text>
+        <Form.Control type="text" name="LastName" required placeholder="Last Name" onChange={handleChange} />
+        <Form.Text className="text-muted">Please enter your last name</Form.Text>
+        <Form.Control type="text" name="UserName" required placeholder="Enter User Name" onChange={handleChange} />
+        <Form.Text className="text-muted">Please enter your user name</Form.Text>
+        <Form.Control type="text" name="Address" required placeholder="Enter Address" onChange={handleChange} />
+        <Form.Text className="text-muted">Please enter your address</Form.Text>
+        <Form.Check
+          className="seller?"
+          type="switch"
+          id="seller"
+          name="IsSeller"
+          checked={formData.IsSeller}
+          onChange={(e) => {
+            setFormData((prevState) => ({
+              ...prevState,
+              IsSeller: e.target.checked,
+            }));
+          }}
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
@@ -31,9 +66,17 @@ function RegisterForm({ user, updateUser }) {
 
 RegisterForm.propTypes = {
   user: PropTypes.shape({
-    uid: PropTypes.string.isRequired,
+    fbUser: PropTypes.shape({
+      id: PropTypes.number,
+      uId: PropTypes.string.isRequired,
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      email: PropTypes.string,
+      userName: PropTypes.string,
+      address: PropTypes.string,
+      isSeller: PropTypes.bool,
+    }).isRequired,
   }).isRequired,
-  updateUser: PropTypes.func.isRequired,
 };
 
 export default RegisterForm;
